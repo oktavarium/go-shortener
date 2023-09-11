fb86a1d669be19fa26731342e92a312cc02e82cc
okta <hided@email.com>
2023-09-11 13:31:36 +0300

    first initial commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,9 @@
 package main
 
-func main() {}
+import "github.com/oktavarium/go-shortener/internal/server"
+
+func main() {
+	if err := server.Run(); err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/oktavarium/go-shortener
+
+go 1.21.0
+
+require github.com/go-chi/chi/v5 v5.0.10
